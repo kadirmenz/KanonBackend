a18f9a1f64736a95f78964659973ae6d6aab6b6d
Kadir <kadirmenzx@gmail.com>
2024-05-16 19:23:21 +0300

    cors package

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(cors({
+    origin: [
+      'https://6645db355f5b3c000925af94--kanongaming-slot.netlify.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  }));
   app.setGlobalPrefix('api');
   const port = process.env.PORT || 3000;
   await app.listen(port);
