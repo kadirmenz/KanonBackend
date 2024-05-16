fd31a64cb9bbbe20598355e13bbdd686b977a9a3
Kadir <kadirmenzx@gmail.com>
2024-05-16 13:13:56 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: [
+      'https://6645db355f5b3c000925af94--kanongaming-slot.netlify.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-  });  
+  });
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
