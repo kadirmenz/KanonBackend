772666e584aa7c605949783984720d32841505d6
Kadir <kadirmenzx@gmail.com>
2024-05-16 19:14:05 +0300

    header seted

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    methods: '*',
-    credentials: true,
-  });  
+  app.enableCors();
   app.setGlobalPrefix('api');
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
