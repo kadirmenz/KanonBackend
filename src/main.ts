bd9d1642d450dbecec66e090403a5e15e9f87c12
Kadir <kadirmenzx@gmail.com>
2024-05-16 18:45:42 +0300

    header set

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,13 @@ async function bootstrap() {
     credentials: true,
   });  
   app.setGlobalPrefix('api');
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
