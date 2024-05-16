b109baf442a7ce22a378f8c3e0b0060eb2547119
Kadir <kadirmenzx@gmail.com>
2024-05-16 19:30:08 +0300

    cors enabled 2 times

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const cors = require("cors");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
     methods: '*',
     credentials: true,
   });  
+  app.use(cors());
   app.setGlobalPrefix('api');
   const port = process.env.PORT || 3000;
   await app.listen(port);
